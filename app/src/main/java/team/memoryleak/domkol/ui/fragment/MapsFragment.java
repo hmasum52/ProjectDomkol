e0f87e1e02b857bf1d3f4a68109993ab85a5be9d
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2022-02-18 15:07:32 +0600

    added google map

---
+++ app/src/main/java/team/memoryleak/domkol/ui/fragment/MapsFragment.java
@@ -0,0 +1,78 @@
+package team.memoryleak.domkol.ui.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.jetbrains.annotations.NotNull;
+
+import team.memoryleak.domkol.R;
+import team.memoryleak.domkol.databinding.FragmentMapsBinding;
+
+/**
+ * @author Hasan Masum
+ *
+ * @see <a href="https://developers.google.com/maps/documentation/android-sdk/start">android map quick start</a>
+ */
+public class MapsFragment extends Fragment implements OnMapReadyCallback{
+
+    public static final String TAG = "MapsFragment->";
+    FragmentMapsBinding mVB;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mVB =  FragmentMapsBinding.inflate(inflater, container, false);
+        return mVB.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initMap();
+    }
+
+    /**
+     * get the map fragment and start the MapAsync
+     */
+    private void initMap() {
+        Log.d(TAG, "initMap: ");
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        try {
+            mapFragment.getMapAsync(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * In this case, we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to
+     * install it inside the SupportMapFragment. This method will only be triggered once the
+     * user has installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(@NotNull GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: google map is ready");
+    }
+}
